890cfd9d48ca38ecd44b81569eb68de4847a0daa
Jared Meeker <jared@meekers.org>
2014-02-11 21:41:55 -0700

    Added missing git fmt commands in build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,9 @@ go install github.com/akavel/rsrc
 bin\rsrc.exe -manifest tardis.exe.manifest -o src/tardis/tardis.syso
 
 go fmt src/tardis/tardis.go
+go fmt src/tardis/popup/*.go
+go fmt src/tardis/logout/*.go
+go fmt src/tardis/userinfo/*.go
 go build -ldflags="-H windowsgui" tardis
-REM go build tardis
 
 pause
